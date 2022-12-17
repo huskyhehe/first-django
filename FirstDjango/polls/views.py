af07f72147ce59e2d4edc5e03e1cdd98192ad585
huskyhehe <zhou.he1@northeastern.edu>
2022-12-17 01:56:08 -0800

    Init commit

--- FirstDjango/polls/views.py
+++ FirstDjango/polls/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Choice, Question
+from .models import Question, Choice
 
 
 def index(request):
